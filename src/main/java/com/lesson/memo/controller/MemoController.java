9eb58ea9da4b457086925d36f299691c25d05660
sakakibararikou <sakakibararikou@localhost>
2026-08-30 00:07:00 +0900

    メモに優先順位機能を追加

--- src/main/java/com/lesson/memo/controller/MemoController.java
+++ src/main/java/com/lesson/memo/controller/MemoController.java
@@ -4,6 +4,9 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,10 +19,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.lesson.memo.model.Memo;
+import com.lesson.memo.model.Priority;
 import com.lesson.memo.repository.MemoRepository;
-
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/memo")
@@ -39,6 +40,13 @@ public class MemoController {
     public String showForm(Model model) {
         model.addAttribute("memo", new Memo());
         return "memo-form";
+    }
+    
+    @GetMapping("/create") // 新規作成画面
+    public String createForm(Model model) {
+        model.addAttribute("memo", new Memo());
+        model.addAttribute("priorities", Priority.values()); 
+        return "memo-form"; 
     }
 
     @PostMapping("/create")
@@ -69,7 +77,9 @@ public class MemoController {
 
     @GetMapping("/edit/{id}")
     public String showEditForm(@PathVariable Long id, Model model, HttpServletResponse response) {
-        if (model.containsAttribute("memo")) {
+    	model.addAttribute("priorities", Priority.values());
+    	
+    	if (model.containsAttribute("memo")) {
             return "memo-form";
         }
 
@@ -107,7 +117,9 @@ public class MemoController {
 
         memoToUpdate.setTitle(memo.getTitle());
         memoToUpdate.setContent(memo.getContent());
+        memoToUpdate.setPriority(memo.getPriority());
         memoToUpdate.setUpdatedAt(LocalDateTime.now());
+        
         memoRepository.save(memoToUpdate);
 
         return "redirect:/memo/detail/" + id;
